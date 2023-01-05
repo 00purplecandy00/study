79f8878b5287e3c4a86a5436e76ffd66b682f2fe
purplecandy <00purplecandy00@gmail.com>
2023-01-05 13:48:22 +0900

    제출

---
+++ OneDrive/바탕 화면/20230105/lotto.py
@@ -0,0 +1,7 @@
+import random
+
+list = []
+for i in range(1,46):
+    list.append(i)
+    
+print(random.sample(list,6))
---
+++ OneDrive/바탕 화면/20230105/shuffle.py
@@ -0,0 +1,5 @@
+import random
+
+students = [1,2,3,4,5,6]
+random.shuffle(students)
+print(students)
