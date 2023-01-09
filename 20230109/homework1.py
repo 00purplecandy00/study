46e53298ee81aaacdc334f74fbef6842fc2ba193
purplecandy <00purplecandy00@gmail.com>
2023-01-09 13:40:39 +0900

    제출1

---
+++ 20230109/homework1.py
@@ -0,0 +1,31 @@
+a1 = int(input())
+print(a1)
+
+a2, b2 = map(int,input().split())
+print(a2,b2)
+
+a3, b3, c3 = map(int,input().split())
+print(a3,b3,c3)
+
+a4, b4, c4 = map(str,input().split())
+print(a4,b4,c4)
+
+a5,b5,c5,d5,e5 = map(int,input().split())
+
+print(a5,b5,c5,d5,e5)
+
+a6, b6 = map(int,input().split())
+print(a6,b6)
+
+a7,b7,c7,d7,e7 = map(int,input().split())
+
+print(a7,b7,c7,d7,e7)
+
+list1 = map(int,input().split())
+for i in list1:
+    print(i,end="")
+
+list2 = map(int,input().split())
+for j in list2:
+    print(j,end="")
+
